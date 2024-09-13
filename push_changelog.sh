d072dc6dba40fa40c83d31c33bbcf6eb4067e039
Mathias Vatter <mvatter@students.uni-mainz.de>
2024-09-13 13:00:54 +0200

    add script

---
+++ push_changelog.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BRANCH="cksp-releases"
+COMMIT_MESSAGE="Update changelogs"
+
+# Überprüfe, ob wir uns auf dem richtigen Branch befinden
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$CURRENT_BRANCH" != "$BRANCH" ]]; then
+    echo "Error: You are not on the branch '$BRANCH'. Please switch to the correct branch." >&2
+    popd >/dev/null
+    exit 1
+fi
+
+# Überprüfe, ob es Änderungen im Arbeitsverzeichnis gibt
+if git diff --quiet && git diff --cached --quiet; then
+    echo "No changes to commit."
+else
+    git add *
+
+    git commit -m "$COMMIT_MESSAGE"
+
+    git push origin "$BRANCH"
+    
+    echo "Changes pushed to '$BRANCH' with message: '$COMMIT_MESSAGE'"
+fi
